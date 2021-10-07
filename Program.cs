2bfe8b134e65c7beded7d8ba2f3f1c7ccb1a5f31
Farhad <farhadalam75@gmail.com>
2021-10-08 00:14:13 +0600

    execute a query on non-generic IEnumerable collections

--- Program.cs
+++ Program.cs
@@ -1,15 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LINQ.NonGeneric
 {
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int[] Scores { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            ArrayList arrayList = new ArrayList
+            {
+                new Student
+                {
+                    FirstName = "Svetlana",
+                    LastName = "Omelchenko",
+                    Scores = new int[] { 98, 92, 81, 60 }
+                },
+                new Student
+                {
+                    FirstName = "Claire",
+                    LastName = "O’Donnell",
+                    Scores = new int[] { 75, 84, 91, 39 }
+                },
+                new Student
+                {
+                    FirstName = "Sven",
+                    LastName = "Mortensen",
+                    Scores = new int[] { 88, 94, 65, 91 }
+                },
+                new Student
+                {
+                    FirstName = "Cesar",
+                    LastName = "Garcia",
+                    Scores = new int[] { 97, 89, 85, 82 }
+                }
+            };
+
+            var query = from Student student in arrayList
+                        where student.Scores[0] > 95
+                        select student;
+
+            foreach (Student s in query)
+                Console.WriteLine(s.LastName + ": " + s.Scores[0]);
+
+            // Keep the console window open in debug mode.  
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
